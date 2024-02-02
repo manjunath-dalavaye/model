126bc5130df0ea2797fc9a1648f0c53cbcdfa9a0
manjunath-dalavaye <manjunathadvg2002@gmail.com>
2024-02-02 14:25:41 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css'
+import { useState } from "react";
+import User from './user';
+
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="App">
+      <h1>User Details Modal</h1>
+      <button onClick={openModal} className="submit-button">
+        Open Form
+      </button>
+      {isOpen && <User closeModal={closeModal} />}
+    </div>
+  );
+};
+
+export default App;
